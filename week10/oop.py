2be23f9afc2b30d981ad7be49581c59a7309922d
Normturtle <omix1.ok@gmail.com>
2024-05-06 21:16:51 +0530

    opp py

---
+++ week10/oop.py
@@ -0,0 +1,14 @@
+# object orientent programming
+# `Class ` keyword defines a `object`
+
+class dog():
+    def bark(self):
+        print('bow')
+    def addd(self,x):
+        return 10+x
+
+
+d = dog()
+d.bark()
+print(d.addd(10))
+# print(dog)
