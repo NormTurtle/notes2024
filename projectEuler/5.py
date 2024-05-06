5d29f6876440e96c561cae6a900f8fbab71fbc3b
Normturtle <omix1.ok@gmail.com>
2024-05-06 21:16:29 +0530

    euler prolem 5

--- projectEuler/5.py
+++ projectEuler/5.py
@@ -1,6 +1,12 @@
-function reverse(n)
-reversed = 0
-while n > 0
-reversed = 10*reversed + n mod 10
-n = n/10
-return reversed
+last = 0
+n = 0
+l = []
+while not n > 100 :
+    n += 1
+    for i in range(2,10,-1): # from 2 to 10
+        if n % i == 0 :
+            last = n
+            l.append(n)
+
+print(last)
+print(l)
